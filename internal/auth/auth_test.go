8c754aed75b74bc82e7a792744350e95a5534fbd
sudonetizen <sardortolibjonov1039@gmail.com>
2025-10-10 16:47:42 +0500

    added style check to workflow

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,46 +1,53 @@
 package auth
 
 import (
-   "fmt"
-   "testing"
-   "net/http"
+	"fmt"
+	"net/http"
+	"testing"
 )
 
 func TestGetAPIKey(t *testing.T) {
 
-    req1, err := http.NewRequest("GET", "http://example.com", nil)
-    if err != nil {fmt.Println("error with creating request 1")}
-    req1.Header.Set("Authorization", "ApiKey  895623")
+	req1, err := http.NewRequest("GET", "http://example.com", nil)
+	if err != nil {
+		fmt.Println("error with creating request 1")
+	}
+	req1.Header.Set("Authorization", "ApiKey  895623")
 
-    req2, err := http.NewRequest("GET", "http://example.com", nil)
-    if err != nil {fmt.Println("error with creating request 2")}
+	req2, err := http.NewRequest("GET", "http://example.com", nil)
+	if err != nil {
+		fmt.Println("error with creating request 2")
+	}
 
-    req3, err := http.NewRequest("GET", "http://example.com", nil)
-    if err != nil {fmt.Println("error with creating request 3")}
-    req3.Header.Set("Authorization", "ApiKey895623")
+	req3, err := http.NewRequest("GET", "http://example.com", nil)
+	if err != nil {
+		fmt.Println("error with creating request 3")
+	}
+	req3.Header.Set("Authorization", "ApiKey895623")
 
+	tests := []struct {
+		req *http.Request
+		err error
+	}{
+		{
+			req: req1,
+			err: nil,
+		},
+		{
+			req: req2,
+			err: ErrNoAuthHeaderIncluded,
+		},
+		{
+			req: req3,
+			err: ErrMalformedAuthzHeader,
+		},
+	}
 
-    tests := []struct{
-        req *http.Request
-        err error 
-    }{
-        {
-            req: req1,
-            err: nil,
-        },
-        {
-            req: req2,
-            err: ErrNoAuthHeaderIncluded,
-        },
-        {
-            req: req3,
-            err: ErrMalformedAuthzHeader,
-        },
-    }
-
-    for _, tst := range tests {
-        _, err := GetAPIKey(tst.req.Header)
-        if err != tst.err {t.Errorf("failed: %v", err)}
-    }    
+	for _, tst := range tests {
+		_, err := GetAPIKey(tst.req.Header)
+		if err != tst.err {
+			t.Errorf("failed: %v", err)
+		}
+	}
 
 }
